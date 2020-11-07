384d0e9bc796d2b68f7b88b3c423e1aad562e8d8
shavit <shavit@users.noreply.github.com>
2020-11-07 16:53:22 -0500

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.js$/
+    })
+
+    return config
+  }
+};
